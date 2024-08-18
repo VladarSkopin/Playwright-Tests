114e9b67acc449cef339d09f7978a6171e54b69b
Alexander Skopintsev <grudagor@gmail.com>
2024-08-18 17:13:17 +0300

    moved CSS locators to a separate file

---
+++ playwright_tests/tests/locators.ts
@@ -0,0 +1,6 @@
+export const paginationLocators = {
+    pageTitle: 'div.header > div.title',
+    btnPagination: 'a.build-button-source',
+    btnPaginationMin: 'a.build-button-zipped',
+    sectionTitleArray: 'div.demo-section-title'
+  };
--- playwright_tests/tests/test_pagination.spec.ts
+++ playwright_tests/tests/test_pagination.spec.ts
@@ -1,17 +1,19 @@
 import { test, expect } from '@playwright/test';
+import { paginationLocators } from './locators';
 
 
-test.describe('Test Pagination', async () => {
+test.describe('Test Pagination', { tag: '@ui' }, async () => {
 
     test.beforeEach(async ({ page }) => {
         await page.goto('https://pagination.js.org/');
     });
 
+    // TODO: split UI checks into separate test steps
     test('General Page Layout', async ({ page }) => {
         // TODO: Move CSS locators to a separate file
 
-        await page.waitForSelector('div.header > div.title', { state: 'visible' });
-        const pageTitle = await page.locator('div.header > div.title');
+        await page.waitForSelector(paginationLocators.pageTitle, { state: 'visible' });
+        const pageTitle = await page.locator(paginationLocators.pageTitle);
         await expect(pageTitle).toContainText('Pagination.js');
         await expect(pageTitle).toHaveCSS('font-size', '60px');
         await expect(pageTitle).toHaveCSS('text-align', 'center');
@@ -25,8 +27,8 @@ test.describe('Test Pagination', async () => {
         await expect(titleDescription).toHaveCSS('text-align', 'center');
         await expect(titleDescription).toHaveCSS('color', 'rgb(255, 255, 255)');
 
-        await page.waitForSelector('a.build-button-source', { state: 'visible' });
-        const btnPagination = await page.locator('a.build-button-source');
+        await page.waitForSelector(paginationLocators.btnPagination, { state: 'visible' });
+        const btnPagination = await page.locator(paginationLocators.btnPagination);
         await expect(btnPagination).toContainText('Pagination.js');
         await expect(btnPagination).toHaveAttribute('href', 'dist/2.6.0/pagination.js');
         await expect(btnPagination).toHaveCSS('display', 'inline-block');
@@ -44,8 +46,8 @@ test.describe('Test Pagination', async () => {
         await expect(btnPagination).toHaveCSS('text-align', 'center');
         await expect(btnPagination).toHaveCSS('line-height', '33.6px');
 
-        await page.waitForSelector('a.build-button-zipped', { state: 'visible' });
-        const btnPaginationMin = await page.locator('a.build-button-zipped');
+        await page.waitForSelector(paginationLocators.btnPaginationMin, { state: 'visible' });
+        const btnPaginationMin = await page.locator(paginationLocators.btnPaginationMin);
         await expect(btnPaginationMin).toContainText('Pagination.min.js');
         await expect(btnPaginationMin).toHaveAttribute('href', 'dist/2.6.0/pagination.min.js');
         await expect(btnPaginationMin).toHaveCSS('display', 'inline-block');
@@ -64,9 +66,9 @@ test.describe('Test Pagination', async () => {
         await expect(btnPaginationMin).toHaveCSS('line-height', '33.6px');
 
         // Wait for all the section titles to load before doing assertions
-        await page.waitForSelector('div.demo-section-title', { state: 'visible' });
+        await page.waitForSelector(paginationLocators.sectionTitleArray, { state: 'visible' });
 
-        const sectionTitle_1 = await page.locator('div.demo-section-title').nth(0);
+        const sectionTitle_1 = await page.locator(paginationLocators.sectionTitleArray).nth(0);
         // TODO: Load section titles' texts from a separate file (collection) -> traverse the array in a loop
         await expect(sectionTitle_1).toHaveText('Normal');
         // TODO: move basic CSS checks to a separate helper funtion (font-size, text-align, box-sizing, color)
@@ -75,98 +77,98 @@ test.describe('Test Pagination', async () => {
         await expect(sectionTitle_1).toHaveCSS('box-sizing', 'border-box');
         await expect(sectionTitle_1).toHaveCSS('color', 'rgb(51, 51, 51)');
 
-        const sectionTitle_2 = await page.locator('div.demo-section-title').nth(1);
+        const sectionTitle_2 = await page.locator(paginationLocators.sectionTitleArray).nth(1);
         await expect(sectionTitle_2).toHaveText('Only page numbers');
         await expect(sectionTitle_2).toHaveCSS('font-size', '40px');
         await expect(sectionTitle_2).toHaveCSS('text-align', 'center');
         await expect(sectionTitle_2).toHaveCSS('box-sizing', 'border-box');
         await expect(sectionTitle_2).toHaveCSS('color', 'rgb(51, 51, 51)');
 
-        const sectionTitle_3 = await page.locator('div.demo-section-title').nth(2);
+        const sectionTitle_3 = await page.locator(paginationLocators.sectionTitleArray).nth(2);
         await expect(sectionTitle_3).toHaveText('Show page size changer');
         await expect(sectionTitle_3).toHaveCSS('font-size', '40px');
         await expect(sectionTitle_3).toHaveCSS('text-align', 'center');
         await expect(sectionTitle_3).toHaveCSS('box-sizing', 'border-box');
         await expect(sectionTitle_3).toHaveCSS('color', 'rgb(51, 51, 51)');
 
-        const sectionTitle_4 = await page.locator('div.demo-section-title').nth(3);
+        const sectionTitle_4 = await page.locator(paginationLocators.sectionTitleArray).nth(3);
         await expect(sectionTitle_4).toHaveText('Show "go" input & button');
         await expect(sectionTitle_4).toHaveCSS('font-size', '40px');
         await expect(sectionTitle_4).toHaveCSS('text-align', 'center');
         await expect(sectionTitle_4).toHaveCSS('box-sizing', 'border-box');
         await expect(sectionTitle_4).toHaveCSS('color', 'rgb(51, 51, 51)');
 
-        const sectionTitle_5 = await page.locator('div.demo-section-title').nth(4);
+        const sectionTitle_5 = await page.locator(paginationLocators.sectionTitleArray).nth(4);
         await expect(sectionTitle_5).toHaveText('Auto hide previous & next button');
         await expect(sectionTitle_5).toHaveCSS('font-size', '40px');
         await expect(sectionTitle_5).toHaveCSS('text-align', 'center');
         await expect(sectionTitle_5).toHaveCSS('box-sizing', 'border-box');
         await expect(sectionTitle_5).toHaveCSS('color', 'rgb(51, 51, 51)');
 
-        const sectionTitle_6 = await page.locator('div.demo-section-title').nth(5);
+        const sectionTitle_6 = await page.locator(paginationLocators.sectionTitleArray).nth(5);
         await expect(sectionTitle_6).toHaveText('Mini');
         await expect(sectionTitle_6).toHaveCSS('font-size', '40px');
         await expect(sectionTitle_6).toHaveCSS('text-align', 'center');
         await expect(sectionTitle_6).toHaveCSS('box-sizing', 'border-box');
         await expect(sectionTitle_6).toHaveCSS('color', 'rgb(51, 51, 51)');
 
-        const sectionTitle_7 = await page.locator('div.demo-section-title').nth(6);
+        const sectionTitle_7 = await page.locator(paginationLocators.sectionTitleArray).nth(6);
         await expect(sectionTitle_7).toHaveText('Show all pages');
         await expect(sectionTitle_7).toHaveCSS('font-size', '40px');
         await expect(sectionTitle_7).toHaveCSS('text-align', 'center');
         await expect(sectionTitle_7).toHaveCSS('box-sizing', 'border-box');
         await expect(sectionTitle_7).toHaveCSS('color', 'rgb(51, 51, 51)');
 
-        const sectionTitle_8 = await page.locator('div.demo-section-title').nth(7);
+        const sectionTitle_8 = await page.locator(paginationLocators.sectionTitleArray).nth(7);
         await expect(sectionTitle_8).toHaveText('Asynchronous or JSONP');
         await expect(sectionTitle_8).toHaveCSS('font-size', '40px');
         await expect(sectionTitle_8).toHaveCSS('text-align', 'center');
         await expect(sectionTitle_8).toHaveCSS('box-sizing', 'border-box');
         await expect(sectionTitle_8).toHaveCSS('color', 'rgb(51, 51, 51)');
 
-        const sectionTitle_9 = await page.locator('div.demo-section-title').nth(8);
+        const sectionTitle_9 = await page.locator(paginationLocators.sectionTitleArray).nth(8);
         await expect(sectionTitle_9).toHaveText('Asynchronous & Dynamic total number');
         await expect(sectionTitle_9).toHaveCSS('font-size', '40px');
         await expect(sectionTitle_9).toHaveCSS('text-align', 'center');
         await expect(sectionTitle_9).toHaveCSS('box-sizing', 'border-box');
         await expect(sectionTitle_9).toHaveCSS('color', 'rgb(51, 51, 51)');
 
-        const sectionTitle_10 = await page.locator('div.demo-section-title').nth(9);
+        const sectionTitle_10 = await page.locator(paginationLocators.sectionTitleArray).nth(9);
         await expect(sectionTitle_10).toHaveText('Specify default');
         await expect(sectionTitle_10).toHaveCSS('font-size', '40px');
         await expect(sectionTitle_10).toHaveCSS('text-align', 'center');
         await expect(sectionTitle_10).toHaveCSS('box-sizing', 'border-box');
         await expect(sectionTitle_10).toHaveCSS('color', 'rgb(51, 51, 51)');
 
-        const sectionTitle_11 = await page.locator('div.demo-section-title').nth(10);
+        const sectionTitle_11 = await page.locator(paginationLocators.sectionTitleArray).nth(10);
         await expect(sectionTitle_11).toHaveText('Format result data');
         await expect(sectionTitle_11).toHaveCSS('font-size', '40px');
         await expect(sectionTitle_11).toHaveCSS('text-align', 'center');
         await expect(sectionTitle_11).toHaveCSS('box-sizing', 'border-box');
         await expect(sectionTitle_11).toHaveCSS('color', 'rgb(51, 51, 51)');
 
-        const sectionTitle_12 = await page.locator('div.demo-section-title').nth(11);
+        const sectionTitle_12 = await page.locator(paginationLocators.sectionTitleArray).nth(11);
         await expect(sectionTitle_12).toHaveText('Another format result data');
         await expect(sectionTitle_12).toHaveCSS('font-size', '40px');
         await expect(sectionTitle_12).toHaveCSS('text-align', 'center');
         await expect(sectionTitle_12).toHaveCSS('box-sizing', 'border-box');
         await expect(sectionTitle_12).toHaveCSS('color', 'rgb(51, 51, 51)');
 
-        const sectionTitle_13 = await page.locator('div.demo-section-title').nth(12);
+        const sectionTitle_13 = await page.locator(paginationLocators.sectionTitleArray).nth(12);
         await expect(sectionTitle_13).toHaveText('Format navigator');
         await expect(sectionTitle_13).toHaveCSS('font-size', '40px');
         await expect(sectionTitle_13).toHaveCSS('text-align', 'center');
         await expect(sectionTitle_13).toHaveCSS('box-sizing', 'border-box');
         await expect(sectionTitle_13).toHaveCSS('color', 'rgb(51, 51, 51)');
 
-        const sectionTitle_14 = await page.locator('div.demo-section-title').nth(13);
+        const sectionTitle_14 = await page.locator(paginationLocators.sectionTitleArray).nth(13);
         await expect(sectionTitle_14).toHaveText('Format "go" input');
         await expect(sectionTitle_14).toHaveCSS('font-size', '40px');
         await expect(sectionTitle_14).toHaveCSS('text-align', 'center');
         await expect(sectionTitle_14).toHaveCSS('box-sizing', 'border-box');
         await expect(sectionTitle_14).toHaveCSS('color', 'rgb(51, 51, 51)');
 
-        const sectionTitle_15 = await page.locator('div.demo-section-title').nth(14);
+        const sectionTitle_15 = await page.locator(paginationLocators.sectionTitleArray).nth(14);
         await expect(sectionTitle_15).toHaveText('Methods & Events');
         await expect(sectionTitle_15).toHaveCSS('font-size', '40px');
         await expect(sectionTitle_15).toHaveCSS('text-align', 'center');
